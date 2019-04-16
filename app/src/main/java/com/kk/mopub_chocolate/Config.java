07b826d200d510c86af010eaae3e0087330dab85
kkawai <kkawai2@gmail.com>
2019-04-15 22:20:24 -0700

    wip

--- app/src/main/java/com/kk/mopub_chocolate/Config.java
+++ app/src/main/java/com/kk/mopub_chocolate/Config.java
@@ -3,6 +3,7 @@ package com.kk.mopub_chocolate;
 class Config {
 
     static String CHOCOLATE_API_KEY = "XqjhRR";
-    static String MOPUB_API_KEY = "12342134234asdfsadf";
+    static String MOPUB_REWARDED_AD_UNIT_ID = "2abd0875d94149fd86c39ade02a6fce5";
+    static String MOPUB_INVIEW_AD_UNIT_ID = "97facec7d0a64405be19384d85e5b0a4";
 
 }
